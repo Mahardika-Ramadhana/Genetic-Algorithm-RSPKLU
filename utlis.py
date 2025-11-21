93f1608f1c7ba0754738e97c75b18d626248db8f
Joseph Greffen Komala <josephgreffenkomala@mail.ugm.ac.id>
2025-11-21 20:09:52 +0700

    ada beberapa yang ku ubahh, di populasi awal, takut ngerubah logic

--- utlis.py
+++ utlis.py
@@ -6,6 +6,11 @@ def load_data(file_path):
     data=[]
     return data
 
+def get_status(a):
+    return "customer or depot or charging station"
+
 def get_dist(a,b): # euclidean distance
     return a+b
 
+def get_demand(customer):
+    return 1
